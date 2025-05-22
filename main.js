b8b3eb6ed3d5d46b498a1b6199466110fbf898a4
Lissandrodev <Luismexico2006@gmail.com>
2025-05-21 18:24:12 -0700

    commit 1

---
+++ main.js
@@ -0,0 +1,28 @@
+function main(categoria) {}
+
+
+//Obtener edad
+let edad = document.getElementById("edad");
+
+// Categorias de peliculas
+let comedia = document.getElementById("C_comedia");
+comedia.addEventListener("click", main("comedia"));
+
+let crimen = document.getElementById("C_crimen");
+terror.addEventListener("click", main("terror"));
+
+let drama = document.getElementById("C_drama");
+drama.addEventListener("click", main("drama"));
+
+let romance = document.getElementById("C_romance");
+romance.addEventListener("click", main("romance"));
+
+let accion = document.getElementById("C_accion");
+document.getElementById("accion");
+
+let ficcion = document.getElementById("C_ficcion");
+ficcion.addEventListener("click", main("ficcion"));
+
+switch ("categoria") {
+
+}
