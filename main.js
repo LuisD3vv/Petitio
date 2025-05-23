9f7615a3e5e171240afa88393b4edef53edcbad5
Lissandrodev <Luismexico2006@gmail.com>
2025-05-22 18:33:31 -0700

    commit 2

--- main.js
+++ main.js
@@ -1,28 +1,81 @@
-function main(categoria) {}
-
-
-//Obtener edad
 let edad = document.getElementById("edad");
 
-// Categorias de peliculas
-let comedia = document.getElementById("C_comedia");
-comedia.addEventListener("click", main("comedia"));
+function cambiar(opcion) {
+  switch (opcion) { //Cada [i] es el indice del elemento que comparte la misma clase, junto con el modificador de eventos del propio javascript
+    case "crimen":
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[0].style.backgroundImage = "url('img/vertical.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[1].style.backgroundImage = "url('img/vertical.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[2].style.backgroundImage = "url('img/vertical.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[3].style.backgroundImage = "url('img/vertical.jpg')";
+      break;
+    case "comedia":
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[0].style.backgroundImage = "url('img/p01gylnm.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[1].style.backgroundImage = "url('img/p01gylnm.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[2].style.backgroundImage = "url('img/p01gylnm.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[3].style.backgroundImage = "url('img/p01gylnm.jpg')";
+      break;
 
-let crimen = document.getElementById("C_crimen");
-terror.addEventListener("click", main("terror"));
+    case "romance":
+      document.getElementsByClassName("categoria_campo"
+      )[0].style.backgroundImage = "url('img/la-propuesta-secuela.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[1].style.backgroundImage = "url('img/la-propuesta-secuela.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[2].style.backgroundImage = "url('img/la-propuesta-secuela.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[3].style.backgroundImage = "url('img/la-propuesta-secuela.jpg')";
+      break;
 
-let drama = document.getElementById("C_drama");
-drama.addEventListener("click", main("drama"));
-
-let romance = document.getElementById("C_romance");
-romance.addEventListener("click", main("romance"));
-
-let accion = document.getElementById("C_accion");
-document.getElementById("accion");
-
-let ficcion = document.getElementById("C_ficcion");
-ficcion.addEventListener("click", main("ficcion"));
-
-switch ("categoria") {
-
+    case "accion":
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[0].style.backgroundImage = "url('img/backgroundAccion.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[1].style.backgroundImage = "url('img/backgroundAccion.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[2].style.backgroundImage = "url('img/backgroundAccion.jpg')";
+      document.getElementsByClassName(
+        "categoria_campo"
+      )[3].style.backgroundImage = "url('img/backgroundAccion.jpg')";
+      break;
+  }
 }
+
+// Se debe de utilizar el funcion y depues la funcion, junto con el argumento
+let crimen = document.getElementById("C_crimen");
+crimen.addEventListener("click", function () {
+  cambiar("crimen");
+});
+let comedia = document.getElementById("C_comedia");
+comedia.addEventListener("click", function () {
+  cambiar("comedia");
+});
+let romance = document.getElementById("C_romance");
+romance.addEventListener("click", function () {
+  cambiar("romance");
+});
+let accion = document.getElementById("C_accion");
+accion.addEventListener("click", function () {
+  cambiar("accion");
+});
